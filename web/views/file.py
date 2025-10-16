7b6451d08147e9d36da826e39fd116ddc09ad46d
CN_Sakura <XXF18374524605@outlook.com>
2025-10-16 23:20:13 +0800

    多层级文件目录实现

--- web/views/file.py
+++ web/views/file.py
@@ -5,16 +5,38 @@ from web import models
 
 
 def file(request, project_id):
-    if request.method == 'GET':
-        form = FolderModelForm()
-        return render(request, 'file.html', {'form': form})
-    form = FolderModelForm(request.POST)
-
     parent_object = None
     folder_id = request.GET.get('folder', '')
     if folder_id.isdecimal():
         parent_object = models.FileRepository.objects.filter(id=int(folder_id), file_type=2,
                                                              project=request.tracer.project).first()
+    # 界面查看
+    if request.method == 'GET':
+
+        # 导航条
+        breadcrumb_list = []
+        parent = parent_object
+        while parent:
+            breadcrumb_list.insert(0, {'id': parent.id, 'name': parent.name})
+            parent = parent.parent
+
+        # 找到目录下所有的文件和文件及返回给前端
+        queryset = models.FileRepository.objects.filter(project_id=request.tracer.project)
+        if parent_object:
+            # 有父级目录证明进入了某个目录
+            file_object_list = queryset.filter(parent=parent_object).order_by('-file_type')
+        else:
+            # 代表在根目录
+            file_object_list = queryset.filter(parent__isnull=True).order_by('-file_type')
+
+        form = FolderModelForm()
+        context = {'form': form,
+                   'file_object_list': file_object_list,
+                   'breadcrumb_list': breadcrumb_list,
+                   }
+        return render(request, 'file.html', context)
+    form = FolderModelForm(request.POST)
+
     if form.is_valid():
         # 文件夹添加
         form.instance.project = request.tracer.project

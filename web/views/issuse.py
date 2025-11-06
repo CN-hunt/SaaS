e8868addab0784dd775d3b6bc4b1be118c1f1c0e
CN_Sakura <XXF18374524605@outlook.com>
2025-11-06 22:16:34 +0800

    条件筛选框CheckBox功能完成

--- web/views/issuse.py
+++ web/views/issuse.py
@@ -5,19 +5,87 @@ from web import models
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.views.decorators.csrf import csrf_exempt
 import json
+from django.utils.safestring import mark_safe
+
+
+class CheckFilter(object):  # 任务筛选款,类可迭代对象前端方便循环
+    def __init__(self, name, data_list, request):
+        self.name = name
+        self.data_list = data_list
+        self.request = request
+
+    def __iter__(self):
+        for item in self.data_list:
+            key = str(item[0])
+            text = item[1]
+            ck = ""
+            # 如果当前用户请求的URL中status和当前循环key相等
+            value_list = self.request.GET.getlist(self.name)
+            if key in value_list:
+                ck = 'checked'  # 在CheckBox中加入checked属性就是默认选中状态
+                value_list.remove(key)
+            else:
+                value_list.append(key)
+
+            # 为自己生成URL
+            # 在当前URL的基础上去增加一项
+            # status=1&age=19
+            from django.http import QueryDict
+            query_dict = self.request.GET.copy()
+            query_dict._mutable = True
+            query_dict.setlist(self.name, value_list)
+            if 'page' in query_dict:
+                query_dict.pop('page')
+
+            param_url = query_dict.urlencode()
+            if param_url:
+                url = "{}?{}".format(self.request.path_info, param_url)  # status=1&status=2&status=3&xx=1
+            else:
+                url = self.request.path_info
+
+            tpl = '<a class="cell" href="{url}"><input type="checkbox" {ck} /><label>{text}</label></a>'
+            html = tpl.format(url=url, ck=ck, text=text)
+            yield mark_safe(html)
 
 
 def issues(request, project_id):
     if request.method == 'GET':
         form = IssueModelForm(request)
 
+        # 问题查询条件构造
+        # ?status=1&status=2&issues_type=1
+        allow_filter_name = ['issues_type', 'status', 'priority']
+        condition = {}
+        for name in allow_filter_name:
+            value_list = request.GET.getlist(name)
+            if not value_list:
+                continue
+            condition["{}__in".format(name)] = value_list
+        """
+        condition = {
+            "status__in":[1,2],
+            'issues_type':[1,]
+        }
+        """
+
         # 分页组件
-        issues_object_list = models.Issues.objects.filter(project_id=project_id)
+        issues_object_list = models.Issues.objects.filter(project_id=project_id).filter(**condition)
         paginator = Paginator(issues_object_list, 5)
         page = request.GET.get('page')
         page_obj = paginator.get_page(page)
 
-        context = {'form': form, 'issues_object_list': page_obj, 'page_obj': page_obj}
+        context = {
+            'form': form,
+            'issues_object_list': page_obj,
+            'page_obj': page_obj,
+
+            'filter_list': [
+                {'title': "问题类型", 'filter': CheckFilter('issues_type', models.IssuesType.objects.filter(project_id=project_id).values_list('id', 'title'), request)},
+                {'title': "状态", 'filter': CheckFilter('status', models.Issues.status_choices, request)},
+                {'title': "优先级", 'filter': CheckFilter('priority', models.Issues.priority_choices, request)},
+            ]
+        }
+
         return render(request, 'issues.html', context)
 
     print(request.POST)
@@ -45,6 +113,7 @@ def issues_record(request, project_id, issues_id):
     # 判断是否可以评论和是否可以操作这个问题
     if request.method == "GET":
         reply_list = models.IssuesReply.objects.filter(issues_id=issues_id, issues__project=request.tracer.project)
+
         # 将queryset转换为json格式
         data_list = []
         for row in reply_list:
@@ -92,8 +161,26 @@ def issues_change(request, project_id, issues_id):
 
     # 1. 数据库字段更新
     # 1.1 文本
-    if name in ["subject", 'desc', 'start_date', 'end_date']:
+    def create_reply_record(content):
+        new_object = models.IssuesReply.objects.create(
+            reply_type=1,
+            issues=issues_object,
+            content=change_record,
+            creator=request.tracer.user,
+        )
+        new_reply_dict = {
+            'id': new_object.id,
+            'reply_type_text': new_object.get_reply_type_display(),
+            'content': new_object.content,
+            'creator': new_object.creator.username,
+            'datetime': new_object.create_datetime.strftime("%Y-%m-%d %H:%M"),
+            'parent_id': new_object.reply_id
+        }
+        return new_reply_dict
 
+    # 1. 数据库字段更新
+    # 1.1 文本
+    if name in ["subject", 'desc', 'start_date', 'end_date']:
         if not value:
             if not field_object.null:
                 return JsonResponse({'status': False, 'error': "您选择的值不能为空"})
@@ -106,18 +193,91 @@ def issues_change(request, project_id, issues_id):
             # 记录：xx更为了value
             change_record = "{}更新为{}".format(field_object.verbose_name, value)
 
-        new_object = models.IssuesReply.objects.create(
-            reply_type=1,
-            issues=issues_object,
-            content=change_record,
-            creator=request.tracer.user,
-        )
-        new_reply_dict = {  # 数据返回前端
-            'id': new_object.id,
-            'reply_type_text': new_object.get_reply_type_display(),
-            'content': new_object.content,
-            'creator': new_object.creator.username,
-            'datetime': new_object.create_datetime.strftime("%Y-%m-%d %H:%M"),
-            'parent_id': new_object.reply_id
-        }
-        return JsonResponse({'status': True, 'data': new_reply_dict})
+        return JsonResponse({'status': True, 'data': create_reply_record(change_record)})
+
+    # 1.2 FK字段（指派的话要判断是否创建者或参与者）
+    if name in ['issues_type', 'module', 'parent', 'assign']:
+        # 用户选择为空
+        if not value:
+            # 不允许为空
+            if not field_object.null:
+                return JsonResponse({'status': False, 'error': "您选择的值不能为空"})
+            # 允许为空
+            setattr(issues_object, name, None)
+            issues_object.save()
+            change_record = "{}更新为空".format(field_object.verbose_name)
+        else:  # 用户输入不为空
+            if name == 'assign':
+                # 是否是项目创建者
+                if value == str(request.tracer.project.creator_id):
+                    instance = request.tracer.project.creator
+                else:
+                    project_user_object = models.ProjectUser.objects.filter(project_id=project_id,
+                                                                            user_id=value).first()
+                    if project_user_object:
+                        instance = project_user_object.user
+                    else:
+                        instance = None
+                if not instance:
+                    return JsonResponse({'status': False, 'error': "您选择的值不存在"})
+
+                setattr(issues_object, name, instance)
+                issues_object.save()
+                change_record = "{}更新为{}".format(field_object.verbose_name, str(instance))  # value根据文本获取到内容
+            else:
+                # 条件判断：用户输入的值，是自己的值。
+                instance = field_object.remote_field.model.objects.filter(id=value, project_id=project_id).first()
+                if not instance:
+                    return JsonResponse({'status': False, 'error': "您选择的值不存在"})
+
+                setattr(issues_object, name, instance)
+                issues_object.save()
+                change_record = "{}更新为{}".format(field_object.verbose_name, str(instance))  # value根据文本获取到内容
+
+        return JsonResponse({'status': True, 'data': create_reply_record(change_record)})
+
+    # 1.3 choices字段
+    if name in ['priority', 'status', 'mode']:
+        selected_text = None
+        for key, text in field_object.choices:  # 判断合法性防止有人传恶意数据
+            if str(key) == value:
+                selected_text = text
+        if not selected_text:
+            return JsonResponse({'status': False, 'error': "您选择的值不存在"})
+
+        setattr(issues_object, name, value)
+        issues_object.save()
+        change_record = "{}更新为{}".format(field_object.verbose_name, selected_text)
+        return JsonResponse({'status': True, 'data': create_reply_record(change_record)})
+
+    # 1.4 M2M字段
+    if name == "attention":
+        # {"name":"attention","value":[1,2,3]}
+        if not isinstance(value, list):
+            return JsonResponse({'status': False, 'error': "数据格式错误"})
+
+        if not value:  # 也是恶意用户的判断
+            issues_object.attention.set(value)
+            issues_object.save()
+            change_record = "{}更新为空".format(field_object.verbose_name)
+        else:
+            # values=["1","2,3,4]  ->   id是否是项目成员（参与者、创建者）
+            # 获取当前项目的所有成员
+            user_dict = {str(request.tracer.project.creator_id): request.tracer.project.creator.username}
+            project_user_list = models.ProjectUser.objects.filter(project_id=project_id)
+            for item in project_user_list:
+                user_dict[str(item.user_id)] = item.user.username
+            username_list = []
+            for user_id in value:
+                username = user_dict.get(str(user_id))
+                if not username:
+                    return JsonResponse({'status': False, 'error': "用户不存在请重新设置"})
+                username_list.append(username)
+
+            issues_object.attention.set(value)
+            issues_object.save()
+            change_record = "{}更新为{}".format(field_object.verbose_name, ",".join(username_list))
+
+        return JsonResponse({'status': True, 'data': create_reply_record(change_record)})
+
+    return JsonResponse({'status': False, 'error': "八嘎"})
